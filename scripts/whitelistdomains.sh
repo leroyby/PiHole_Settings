019763008342291f472cf8c25e49b3f116d0c404
Leroy Byland <5134519-leroyby@users.noreply.gitlba.com>
2020-05-11 20:42:23 +0200

    Update Script for Pihole V5

--- scripts/whitelistdomains.sh
+++ scripts/whitelistdomains.sh
@@ -4,11 +4,15 @@
 # Created by Anudeep (small changes leroyby)
 # thanks to Anudeep (https://github.com/anudeepND)
 #================================================================================
+
 TICK="[\e[32m ✔ \e[0m]"
+PIHOLE_LOCATION="/etc/pihole"
+GRAVITY_UPDATE_COMMAND="pihole -w -q"
 
-
-echo -e " \e[1m Script will download and add domains from the repo to whitelist.txt of the PiHole \e[0m"
-sleep 0.1
+echo -e " \e[1m This script will download and add domains from the repo to whitelist.txt \e[0m"
+echo -e "\n"
+echo -e " \e[1m All the domains in this list are safe to add and doesn't contain any tracking or adserving domains. \e[0m"
+sleep 1
 echo -e "\n"
 
 if [ "$(id -u)" != "0" ] ; then
@@ -16,18 +20,14 @@ if [ "$(id -u)" != "0" ] ; then
 	exit 2
 fi
 
-curl -sS https://raw.githubusercontent.com/leroyby/PiHole_Settings/master/domains/whitelistedomains.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
-echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
+curl -sS https://raw.githubusercontent.com/leroyby/PiHole_Settings/master/domains/whitelistedomains.txt | sudo tee -a "${PIHOLE_LOCATION}"/whitelist.txt >/dev/null
+echo -e " ${TICK} \e[32m Adding domains to whitelist... \e[0m"
 sleep 0.1
 echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
-mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+mv "${PIHOLE_LOCATION}"/whitelist.txt "${PIHOLE_LOCATION}"/whitelist.txt.old && cat "${PIHOLE_LOCATION}"/whitelist.txt.old | sort | uniq >> "${PIHOLE_LOCATION}"/whitelist.txt
 
 echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-pihole -g > /dev/null
-
-
-echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
-echo -e " ${TICK} \e[32m Done! \e[0m"
+${GRAVITY_UPDATE_COMMAND} $(cat /etc/pihole/whitelist.txt | xargs) > /dev/null
 
 cd /home/pi
 sudo rm -rf PiHole_Settings/
